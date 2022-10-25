a4d2dc0b80b24c057cad32b318c0d7af7281c74e
xtayex <75450574@qq.com>
2022-10-25 11:12:18 +0800

    fix:some bug in run_music.sh. Fuck bash!

--- xdg_config_home/.config/xmobar/run_music.sh
+++ xdg_config_home/.config/xmobar/run_music.sh
@@ -3,6 +3,7 @@
 pre_title="N/A"
 pre_artist="N/A"
 if [ ! -p /tmp/xmobar_mpd_pipe ]; then
+    echo "no pipe"
     mknod /tmp/xmobar_mpd_pipe p
 fi
 if [[ -n $(playerctl -l) ]]; then
@@ -14,11 +15,17 @@ if [[ -n $(playerctl -l) ]]; then
         if playerctl metadata 2>&- | grep artist > /dev/null; then
             artist=$(playerctl metadata artist)
         fi
-        if [[ $title != $pre_title && $artist != $pre_artist ]]; then
-            . $HOME/.config/xmobar/music.sh --all
+        echo "pre_title: $pre_title"
+        echo "pre_artist: $pre_artist"
+        echo "title: $title"
+        echo "artist: $artist"
+        if [ "$title" != "$pre_title" ] || [ "$artist" != "$pre_artist" ]; then
+            echo "not equal"
+            . "$HOME"/.config/xmobar/music.sh --all
             pre_title=$title
             pre_artist=$artist
         fi
+        echo "----------------------------------------------"
         sleep 5
     done
 fi
